2b8ff6f9892a0d59f71d495dbc9b5e29583125d8
Toni-Arjona-EEBE <toni.arjona@estudiantat.upc.edu>
2025-04-04 21:46:28 +0200

    Add files via upload

---
+++ entregable_2_ECI.m
@@ -0,0 +1,19 @@
+clear
+format long
+clc
+
+a = 10;
+b = 5;
+
+c = a/30;
+d = b/30;
+
+Flecha = funcion_entregable_2(a,b,c,d,  2/3*a  ,  b/2  );
+
+fprintf("+----------- Método de Levy ------------+\n");
+fprintf("| [ c = %.8f | d = %.8f ]\t|\n", c, d);
+fprintf("| W(2a/3,b/2) = %.8f m\t\t\t|\n", Flecha);
+fprintf("| W(2a/3,b/2) = %.8f mm\t\t\t|\n", Flecha*10e3);
+fprintf("+---------------------------------------+\n");
+
+
---
+++ funcion_entregable_2.m
@@ -0,0 +1,25 @@
+function Flecha = funcion_entregable_2(a,b,c,d, x, y)
+    D = 2.5*10^7 * 0.25^3 / (12*(1-0.3^2));
+        
+    Matriz = [ 0               1                b*exp(-pi*b/a)     exp(-pi*b/a)
+               b*exp(-pi*b/a)  exp(-pi*b/a)     0               1               
+               -2*pi/a         (pi/a)^2         (-2*pi/a + b*(pi/a)^2)*exp(-pi*b/a)     (pi/a)^2*exp(-pi*b/a)
+                (-2*pi/a + b*(pi/a)^2)*exp(-pi*b/a)     (pi/a)^2*exp(-pi*b/a)  (-2*pi/a)    (pi/a)^2   ];
+    
+
+    Pn = 8*sin(pi*c/(2*a))/(pi*c*d);
+    k = -a^4*Pn/(D*pi^4);
+    
+    Iguales = transpose([k k 0 0]);
+    
+    coef = Matriz\Iguales;
+
+
+    %display(Matriz);
+    %display(Iguales);
+    %display(coef);
+
+    fFlecha = @(s,t) ( coef(1)*t*exp(-pi*t/a) + coef(2)*exp(-pi*t/a) + coef(3)*(b-t)*exp(-pi*(b-t)/a) + coef(4)*t*exp(-pi*(b-t)/a) + (a^4*Pn)/(D*pi^4) )* sin(pi*s/a);
+    
+    Flecha = fFlecha(x,y);
+end
